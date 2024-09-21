2eda81c34b076b0e74d942b930425c251211f98e
apykhanov <121295115+apykhanov@users.noreply.github.com>
2024-09-21 21:02:31 +0300

    Адаптивная сетка

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+const headerNav = document.querySelector('.header-nav');
+const toggle = document.querySelector('.header-nav__toggle');
+const siteList = document.querySelector('.site-list');
+
+
+siteList.classList.remove('site-list--opened');
+
+toggle.addEventListener('click', () => {
+  headerNav.classList.toggle('header-nav--closed');
+  siteList.classList.toggle('site-list--opened');
+});
